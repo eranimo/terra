f71201691f45d8f74b3c00f9e977df7c6069df4d
Kaelan Cooter <dcgamers@gmail.com>
2019-09-15 16:52:30 -0400

    all css in js

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,20 @@ import { Controls } from './components/Controls';
 import { EDrawMode, EMapMode, IDrawOptions, IGlobeOptions } from './types';
 import { loadImages } from './utils';
 import { GameManager } from './GameManager';
+import { CircularProgress, Flex, Spinner, Box } from '@chakra-ui/core';
+
+
+const LoadingOverlay = () => (
+  <Flex
+    justify="center"
+    align="center"
+    position="fixed"
+    zIndex={10000}
+    left={0} right={0} top={0} bottom={0}
+  >
+    <Spinner size="xl" />
+  </Flex>
+);
 
 
 (window as any)._ = require('lodash');
@@ -32,21 +46,38 @@ export function App() {
   }, []);
 
   const { width, height } = useWindowSize();
-
   return (
     <div>
-      {isLoading && <div id="loading">Loading...</div>}
+      {isLoading && <LoadingOverlay />}
       <canvas
         ref={screenRef}
         width={width}
         height={height}
       />
-      <canvas
-        className="minimap"
-        ref={minimapRef}
-        width={360 * 5}
-        height={180 * 5}
-      />
+      <Box
+        bg="black"
+        borderWidth="1px"
+        borderColor="gray.600"
+        position="fixed"
+        right={0}
+        bottom={0}
+        width={360}
+        height={180}
+        style={{
+          visibility: isLoading ? 'hidden' : 'visible'
+        }}
+      >
+        <canvas
+          ref={minimapRef}
+          width={360 * 5}
+          height={180 * 5}
+          style={{
+            transform: 'rotate(180deg)',
+            width: '360px',
+            height: '180px',
+          }}
+        />
+      </Box>
       {!isLoading && <Controls manager={manager} />}
     </div>
   );
